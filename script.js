bed59cbdcd814a0eeedc14286e9975f4388b4219
OlegikSSoN <olegiksson@gmail.com>
2023-02-01 15:29:08 +0200

    button

---
+++ script.js
@@ -0,0 +1,8 @@
+const parentBlock = document.getElementsByClassName("parent-block");
+
+for (const element of parentBlock) {
+  const innerBtn = document.createElement("button");
+  innerBtn.className = "inner-block";
+  innerBtn.innerText = "Push me";
+  element.appendChild(innerBtn);
+}

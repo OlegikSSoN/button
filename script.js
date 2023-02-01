676f77076c912379d701a5ba4ccf1c036dc08821
OlegikSSoN <olegiksson@gmail.com>
2023-02-01 18:23:52 +0200

    button

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const wrapperBlock = document.getElementsByClassName("parent-block");
 
-for (const element of wrapperBlock) {
+for (const button of wrapperBlock) {
   let btn = document.createElement("button");
-  btn.innerText = "Save";
   btn.style.width = "80%";
   btn.style.padding = "2px";
   btn.style.margin = "20px";
-  element.appendChild(btn);
+  btn.textContent = "Save";
+  button.appendChild(btn);
 }
